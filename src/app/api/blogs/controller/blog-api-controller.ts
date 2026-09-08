7d7119270406a0c6dce4573d544f151c7f5aa453
Mason Karr <karr.mason@gmail.com>
2026-09-08 12:19:09 -0500

    Created getAllByUsername, patchEditArticle, and deleteById functions;  with relative switch cases and controller functions

--- src/app/api/blogs/controller/blog-api-controller.ts
+++ src/app/api/blogs/controller/blog-api-controller.ts
@@ -9,12 +9,24 @@ export async function fetchAll(): Promise<any> {
    return fetchData(ROUTE_URL_PREFIX, {route: 'all'});
 }
 
+export async function fetchAllByUsername():Promise <any> {
+    return fetchData(ROUTE_URL_PREFIX, {route: 'allbyuser'})
+}
+
 export async function fetchByID(ID: string): Promise<any> {
     return fetchData("http://localhost:3000/api/blogs/", {route: 'id', ID});
 }
 
 export async function postNewArticle(body: any): Promise<any> {
     return fetchData(ROUTE_URL_PREFIX, {route: 'new', body})
+}
+
+export async function editArticle(body:any): Promise<any>{
+    return fetchData(ROUTE_URL_PREFIX, {route: 'edit', body});
+}
+
+export async function deleteArticle(ID:string): Promise<any>{
+    return fetchData(ROUTE_URL_PREFIX, {route: 'delete', ID})
 }
 
 async function fetchData(url: string, body: any) {
